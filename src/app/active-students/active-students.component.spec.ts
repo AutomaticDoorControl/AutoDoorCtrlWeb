38a6d36e4b1d7a22043826f2e0ed62d7f4fa01b8
Brandon Sakai <brandon.sakai@gmail.com>
2019-10-09 23:26:53 +0000

    Added tests for ActiveStudentsComponent

--- src/app/active-students/active-students.component.spec.ts
+++ src/app/active-students/active-students.component.spec.ts
@@ -1,15 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RouterTestingModule } from '@angular/router/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { Observable } from 'rxjs/Observable';
 
 import { AdminService } from '../admin.service';
 import { StudentService } from '../student.service';
 import { ActiveStudentsComponent } from './active-students.component';
+import { apiServer } from '../globals';
 
 describe('ActiveStudentsComponent', () => {
   let component: ActiveStudentsComponent;
   let fixture: ComponentFixture<ActiveStudentsComponent>;
+  let StudentMock = jasmine.createSpyObj('StudentService', ['getStudents', 'getActive', 'remove', 'logout']);
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -18,9 +21,18 @@ describe('ActiveStudentsComponent', () => {
 	HttpClientTestingModule
       ],
       declarations: [ ActiveStudentsComponent ],
-      providers: [ AdminService, StudentService ]
+      providers: [ AdminService, {provide:StudentService, useValue:StudentMock} ]
     })
     .compileComponents();
+    StudentMock.getActive.and.returnValue(
+      new Observable( (observer) => {
+        observer.next(
+	  [{"RCSid":"userOne","Status":"Active"},
+	  {"RCSid":"userTwo","Status":"Active"},
+	  {"RCSid":"userThree","Status":"Active"},
+	  {"RCSid":"userFour","Status":"Active"}]
+	)
+    }));
   }));
 
   beforeEach(() => {
@@ -32,4 +44,15 @@ describe('ActiveStudentsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should make remove request when button is clicked', () => {
+    const hostElement = fixture.nativeElement;
+    let submitButton: HTMLElement = hostElement.querySelector('button');
+    submitButton.click();
+    expect(StudentMock.remove).toHaveBeenCalledWith("userOne");
+    submitButton = hostElement.querySelectorAll('button')[2];
+    submitButton.click();
+    expect(StudentMock.remove).toHaveBeenCalledWith("userThree");
+  });
+
 });
