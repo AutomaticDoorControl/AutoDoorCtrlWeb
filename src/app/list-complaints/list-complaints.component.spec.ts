9b0dc5b2b0ae458e620ecc5d9b1c7263a537202b
Brandon Sakai <brandon.sakai@gmail.com>
2019-10-10 03:37:13 +0000

    Added test for list-complaints

--- src/app/list-complaints/list-complaints.component.spec.ts
+++ src/app/list-complaints/list-complaints.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Observable } from 'rxjs/Observable';
 
 import { AdminService } from '../admin.service';
 import { StudentService } from '../student.service';
@@ -10,6 +11,7 @@ import { ListComplaintsComponent } from './list-complaints.component';
 describe('ListComplaintsComponent', () => {
   let component: ListComplaintsComponent;
   let fixture: ComponentFixture<ListComplaintsComponent>;
+  let StudentMock = jasmine.createSpyObj('StudentService', ['listComplaints']);
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -18,9 +20,18 @@ describe('ListComplaintsComponent', () => {
 	HttpClientTestingModule
       ],
       declarations: [ ListComplaintsComponent ],
-      providers: [ AdminService, StudentService ]
+      providers: [ AdminService, {provide: StudentService, useValue: StudentMock} ]
     })
     .compileComponents();
+    StudentMock.listComplaints.and.returnValue(
+      new Observable( (observer) => {
+        observer.next(
+	  [{"location":"locOne","message":"messOne"},
+	  {"location":"locTwo","message":"messTwo"},
+	  {"location":"locThree","message":"messThree"},
+	  {"location":"locFour","message":"messFour"}]
+	)
+    }));
   }));
 
   beforeEach(() => {
@@ -31,5 +42,15 @@ describe('ListComplaintsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should populate table with values', () => {
+    const hostElement = fixture.nativeElement;
+    let table: HTMLElement = hostElement.querySelector('table');
+    let nums = ["One", "Two", "Three", "Four"];
+    for(var i = 1; i < 4; ++i) {
+      expect(table.children[i].children[0].innerHTML).toBe('loc' + nums[i - 1]);
+      expect(table.children[i].children[1].innerHTML).toBe('mess' + nums[i - 1]);
+    }
   });
 });
