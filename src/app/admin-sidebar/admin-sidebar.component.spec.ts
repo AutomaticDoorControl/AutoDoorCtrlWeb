1bdaae87ad4d177d99e82ccaf6d0ae3c74e1e7f1
Brandon Sakai <brandon.sakai@gmail.com>
2019-10-23 01:17:30 +0000

    Added and fixed tests

--- src/app/admin-sidebar/admin-sidebar.component.spec.ts
+++ src/app/admin-sidebar/admin-sidebar.component.spec.ts
@@ -22,4 +22,35 @@ describe('AdminSidebarComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should create one button for each input', () => {
+    component.buttons = ["one", "two", "three"];
+    fixture.detectChanges();
+    const hostElement = fixture.nativeElement;
+    expect(hostElement.querySelectorAll('button').length).toBe(3);
+  });
+
+  it('should create buttons with the names of the inputs', () => {
+    let buttonsArr = ["one", "two", "three"];
+    component.buttons = buttonsArr;
+    fixture.detectChanges();
+    const hostElement = fixture.nativeElement;
+    let buttons = hostElement.querySelectorAll('button');
+    for(var i = 0; i < 3; ++i)
+      expect(buttons[i].innerHTML).toBe(buttonsArr[i]);
+  });
+ 
+  it('should create buttons with the names of the inputs', () => {
+    let buttonsArr = ["one", "two", "three"];
+    component.buttons = buttonsArr;
+    fixture.detectChanges();
+    const hostElement = fixture.nativeElement;
+    let buttons = hostElement.querySelectorAll('button');
+    spyOn(component.clicker, 'emit');
+    for(var i = 0; i < 3; ++i)
+    {
+      buttons[i].click();
+      expect(component.clicker.emit).toHaveBeenCalledWith(buttonsArr[i]);
+    }
+  });
 });
