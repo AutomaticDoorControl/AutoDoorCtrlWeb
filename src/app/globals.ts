cdf73b90dae59b24c3b09ff014cd2c0a222020ff
Brandon Sakai <brandon.sakai@gmail.com>
2020-03-27 19:57:44 +0000

    Typo and updates

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -1,4 +1,3 @@
 'use strict'
 
-//export const apiServer: string = "https://rpiadc.com"
-export const apiServer: string = "http://192.168.56.101";
+export const apiServer: string = "https://rpiadc.com"
